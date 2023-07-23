dbe9fee3eb4bdaae302644b83415602ea3b29498
Vishal Manam <88299493+VishalManam@users.noreply.github.com>
2023-07-23 20:15:32 +0530

    Delete userpanel directory

--- userpanel/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_destroy();
-unset($_SESSION['id']);
-unset($_SESSION['username']);
-unset($_SESSION['email']);
-unset($_SESSION['phone_number']);
-unset($_SESSION['passing_year']);
-unset($_SESSION['verify']);
-header("location: login.php");
